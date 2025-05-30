0865070594d78aaa8b6fc17db8610ce53ff0452c
AluisioPereira <aluisio1102@gmail.com>
2025-05-30 14:58:04 -0300

    Delete codigos/cliente directory

--- codigos/cliente/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'its-redu',
-  webDir: 'dist'
-};
-
-export default config;
